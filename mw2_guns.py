e62fda5d883c04bbc7a4e1612da2801a9dc444c5
Thomas W <80714648+skeyhd@users.noreply.github.com>
2023-03-04 14:25:24 +0000

    Create mw2_guns.py

---
+++ mw2_guns.py
@@ -0,0 +1,57 @@
+# mw2_guns.py
+
+iso_hemlock = {
+    # Info
+    "Name" : "Iso Hemlock",
+    "Gun Type" : "AR",
+    "Gun Platform" : "ISO",
+    "Tier" : "S\n",
+
+    # Muzzle
+    "Muzzle" : "XTEN Havoc 90",
+    "Muzzle Tunning 1" : "+0.57",
+    "Muzzle Tunning 2" : "+0.28\n",
+    # Barrel
+    "Barrel" : "Fielder-T50",
+    "Barrel Tunning 1" : "+0.29",
+    "Barrel Tunning 2" : "+0.40\n",
+    # Optic
+    "Optic" : "Cronen Mini Pro",
+    "Optic Tunning 1" : "-3.00",
+    "Optic Tunning 2" : "-2.25\n",
+    # Stock
+    "Stock" : "Mace 600",
+    "Stock Tunning 1" : "-2.97",
+    "Stock Tunning 2" : "+1.55\n",
+    # Ammo
+    "Ammo" : "5.56 High Velocity",
+    "Ammo Tunning 1" : "±0.00",
+    "Ammo Tunning 2" : "+6.39\n"
+}
+
+
+lachmann_sub = {
+    # Info
+    "Name" : "Lachmann Sub",
+    "Gun Type" : "SMG",
+    "Gun Platform" : "Lachmann & Meer",
+    "Tier" : "S\n",
+    
+    # Barrel
+    "Barrel" : 'FTAC M-Sub 12"',
+    "Barrel Tunning 1" : "+0.50",
+    "Barrel Tunning 2" : "-0.40\n",
+    # Laser
+    "Laser" : "VLK LZR 7mW",
+    "Laser Tunning 1" : "-0.50",
+    "Laser Tunning 2" : "-43.86\n",
+    # Stock
+    "Stock" : "LM Stockless Mod\n",
+    # Magazine
+    "Magazine" : "40 Round Mag\n",
+    # Rear Grip
+    "Rear Grip" : "Lachmann TCG-10",
+    "Rear Grip Tunning 1" : "-0.90",
+    "Rear Grip Tunning 2" : "-0.45\n",
+
+}
