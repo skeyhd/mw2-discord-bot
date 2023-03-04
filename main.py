909dfef08fe7470ca8653124f06c342ed28adfd2
Thomas W <80714648+skeyhd@users.noreply.github.com>
2023-03-04 16:28:51 +0000

    Update main.py

--- main.py
+++ main.py
@@ -27,10 +27,49 @@ async def on_message(message):
     if message.author == client.user:
         return
 
+    if message.content.startswith("!help"):
+        help_command = "!iso hemlock \n!lachmann sub \n!kv broadside \n!sakin mg38 \n!raal mg"
+        
+        await message.channel.send(help_command)
+
+
+    # Iso Hemlock
     if message.content.startswith("!iso hemlock"):
         stats = ""
         for item in iso_hemlock:
             stats = stats + "{} : {}\n".format(item,iso_hemlock[item])
+        
+        await message.channel.send(stats)
+    
+    # Lachmann Sub
+    if message.content.startswith("!lachmann sub"):
+        stats = ""
+        for item in lachmann_sub:
+            stats = stats + "{} : {}\n".format(item,lachmann_sub[item])
+        
+        await message.channel.send(stats)
+    
+    # KV Broadside
+    if message.content.startswith("!kv broadside"):
+        stats = ""
+        for item in kv_broadside:
+            stats = stats + "{} : {}\n".format(item,kv_broadside[item])
+        
+        await message.channel.send(stats)
+    
+    # Sakin MG38
+    if message.content.startswith("!sakin mg38"):
+        stats = ""
+        for item in sakin_mg38:
+            stats = stats + "{} : {}\n".format(item,sakin_mg38[item])
+        
+        await message.channel.send(stats)
+    
+    # Raal MG
+    if message.content.startswith("!raal mg"):
+        stats = ""
+        for item in raal_mg:
+            stats = stats + "{} : {}\n".format(item,raal_mg[item])
         
         await message.channel.send(stats)
             
