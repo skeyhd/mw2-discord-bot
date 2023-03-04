e3ad1a111a34c31288e56b963d39188554c95898
Thomas Wells <80714648+skeyhd@users.noreply.github.com>
2023-03-04 20:49:57 +0000

    Update mw2_guns.py

--- mw2_guns.py
+++ mw2_guns.py
@@ -1,5 +1,6 @@
 # mw2_guns.py
 
+# Iso Hemlock
 iso_hemlock = {
     # Info
     "Name" : "Iso Hemlock",
@@ -29,7 +30,7 @@ iso_hemlock = {
     "Ammo Tunning 2" : "+6.39\n"
 }
 
-
+# Lachmann Sub
 lachmann_sub = {
     # Info
     "Name" : "Lachmann Sub",
@@ -53,9 +54,9 @@ lachmann_sub = {
     "Rear Grip" : "Lachmann TCG-10",
     "Rear Grip Tunning 1" : "-0.90",
     "Rear Grip Tunning 2" : "-0.45\n",
-
 }
 
+# KV Broadside
 kv_broadside = {
     # Info
     "Name" : "KV Broadside",
@@ -81,6 +82,7 @@ kv_broadside = {
     "Bolt" : "Dashbolt 60",
 }
 
+# Sakin MG38
 sakin_mg38 = {
     # Info
     "Name" : "Sakin MG",
@@ -108,9 +110,9 @@ sakin_mg38 = {
     "Ammunition" : "7.62 High Velocity",
     "Ammo Tunning 1" : "-0.70",
     "Ammo Tunning 2" : "-9.00\n",
-
 }
 
+# RAAL MG
 raal_mg = {
     # Info
     "Name" : "Raal MG",
@@ -138,5 +140,258 @@ raal_mg = {
     "Ammunition" : ".338 Mag High Velocity",
     "Ammo Tunning 1" : "-0.70",
     "Ammo Tunning 2" : "-4.94\n",
+}
 
+# TAQ 56
+taq_56 = {
+    # Info
+    "Name" : "TAQ 56",
+    "Gun Type" : "AR",
+    "Gun Platform" : "Tactique Verte",
+    "Tier" : "A\n",
+    
+    # Muzzle
+    "Muzzle" : "Harbinger D20",
+    "Muzzle Tunning 1" : "-0.72",
+    "Muzzle Tunning 2" : "+0.42\n",
+    # Underbarrel
+    "Underbarrel" : "Edge-47 Grip",
+    "Underbarrel Tunning 1" : "+0.41",
+    "Underbarrel Tunning 2" : "+0.22\n",
+    # Ammunition
+    "Ammunition" : "5.56 High Velocity",
+    "Ammo Tunning 1" : "+0.27",
+    "Ammo Tunning 2" : "+5.81\n",
+    # Magazine
+    "Magazine" : "40 Round Mag",
+    # Ammunition
+    "Ammunition Grip" : ".338 Mag High Velocity",
+    "Ammo Tunning 1" : "+0.55",
+    "Ammo Tunning 2" : "+0.26\n",
+}
+
+# Vaznev 9K
+vaznev_9k = {
+    # Info
+    "Name" : "Vaznev 9k",
+    "Gun Type" : "SMG",
+    "Gun Platform" : "Kastovia",
+    "Tier" : "A\n",
+    
+    # Muzzle
+    "Muzzle" : "Lockshot KT85",
+    "Muzzle Tunning 1" : "-0.80",
+    "Muzzle Tunning 2" : "+0.35\n",
+    # Stock
+    "Stock" : "Otrezat Stock",
+    "Stock Tunning 1" : "-4.00",
+    "Stock Tunning 2" : "-2.40\n",
+    # Underbarrel
+    "Underbarrel" : "FSS Sharkfin 90",
+    "Underbarrel Tunning 1" : "-0.77",
+    "Underbarrel Tunning 2" : "-0.40\n",
+    # Magazine
+    "Magazine" : "45 Round Mag",
+    # Rear Grip
+    "Rear Grip" : ".338 Mag High Velocity",
+    "Rear Grip Tunning 1" : "-0.94",
+    "Rear Grip Tunning 2" : "-0.45\n",
+}
+
+# M4
+m4 = {
+    # Info
+    "Name" : "M4",
+    "Gun Type" : "SMG",
+    "Gun Platform" : "Kastovia",
+    "Tier" : "A\n",
+    
+    # Barrel
+    "Barrel" : 'Hightower 20" Barrel',
+    "Barrel Tunning 1" : "+0.50",
+    "Barrel Tunning 2" : "+0.39\n",
+    # Underbarrel
+    "Underbarrel" : "FTAC Ripper 56",
+    "Underbarrel Tunning 1" : "+0.65",
+    "Underbarrel Tunning 2" : "+0.28\n",
+    # Ammunition
+    "Ammunition" : "5.56 High Velocity",
+    "Ammo Tunning 1" : "-0.70",
+    "Ammo Tunning 2" : "-9.00\n",
+    # Magazine
+    "Magazine" : "60 Round Mag",
+    # Rear Grip
+    "Rear Grip" : "Sakin ZX Grip",
+    "Rear Grip Tunning 1" : "+0.97",
+    "Rear Grip Tunning 2" : "+0.45\n",
+}
+
+# RPK
+rpk = {
+    # Info
+    "Name" : "RPK",
+    "Gun Type" : "LMG",
+    "Gun Platform" : "Kastovia",
+    "Tier" : "A\n",
+    
+    # Muzzle
+    "Muzzle" : 'ZLR Talon 5',
+    "Muzzle Tunning 1" : "+1.40",
+    "Muzzle Tunning 2" : "+1.00\n",
+    # Optic
+    "Optic" : "Aim OP-V4",
+    # Underbarrel
+    "Underbarrel" : "FTAC Ripper 56",
+    "Underbarrel Tunning 1" : "+0.80",
+    "Underbarrel Tunning 2" : "+0.40\n",
+    # Ammunition
+    "Ammunition" : "7.62 High Velocity",
+    "Ammo Tunning 1" : "+0.70",
+    "Ammo Tunning 2" : "+9.00\n",
+    # Rear Grip
+    "Rear Grip" : "Demo-X2 Grip",
+    "Rear Grip Tunning 1" : "+1.00",
+    "Rear Grip Tunning 2" : "+0.45\n",
+}
+
+# Signal 50
+signal_50 = {
+    # Info
+    "Name" : "Signal 50",
+    "Gun Type" : "Sniper",
+    "Gun Platform" : "Signal",
+    "Tier" : "A\n",
+    
+    # Barrel
+    "Barrel" : '21.5" Fluted Fifty',
+    "Barrel Tunning 1" : "-0.19",
+    "Barrel Tunning 2" : "+0.09\n",
+    # Optic
+    "Optic" : "SP-X 80 6.6x",
+    "Optic Tunning 1" : "-1.55",
+    "Optic Tunning 2" : "-0.39\n",
+    # Stock
+    "Stock" : "SO Inline Stock",
+    "Stock Tunning 1" : "-3.10",
+    "Stock Tunning 2" : "-1.94\n",
+    # Ammunition
+    "Ammunition" : ".50 Cal High Velocity",
+    "Ammo Tunning 1" : "+0.52",
+    "Ammo Tunning 2" : "+5.52\n",
+    # Rear Grip
+    "Rear Grip" : "SA Finesse Grip",
+    "Rear Grip Tunning 1" : "-0.52",
+    "Rear Grip Tunning 2" : "-0.29\n",
+}
+
+# Kastov 762
+kastov_762 = {
+    # Info
+    "Name" : "Kastov 762",
+    "Gun Type" : "AR",
+    "Gun Platform" : "Kastovia",
+    "Tier" : "A\n",
+    
+    # Muzzle
+    "Muzzle" : 'ZLR Talon 5',
+    "Muzzle Tunning 1" : "+1.40",
+    "Muzzle Tunning 2" : "+1.00\n",
+    # Optic
+    "Optic" : "Slimline Pro",
+    "Optic Tunning 1" : "-3.00",
+    "Optic Tunning 2" : "-2.25\n",
+    # Underbarrel
+    "Underbarrel" : "SO Inline Stock",
+    "Underbarrel Tunning 1" : "+0.80",
+    "Underbarrel Tunning 2" : "+0.40\n",
+    # Magazine
+    "Magazine" : "40 Round Mag",
+    # Rear Grip
+    "Rear Grip" : "True-TAC Grip",
+    "Rear Grip Tunning 1" : "-0.94",
+    "Rear Grip Tunning 2" : "-0.45\n",
+}
+
+# M13B
+m13b = {
+    # Info
+    "Name" : "M13B",
+    "Gun Type" : "AR",
+    "Gun Platform" : "Bruen Ops",
+    "Tier" : "A\n",
+    
+    # Muzzle
+    "Muzzle" : 'Harbinger D20',
+    "Muzzle Tunning 1" : "+1.26",
+    "Muzzle Tunning 2" : "-0.71\n",
+    # Barrel
+    "Barrel" : '14" Bruen Echelon',
+    "Barrel Tunning 1" : "+0.48",
+    "Barrel Tunning 2" : "+0.39\n",
+    # Optic
+    "Optic" : "Aim OP-V4",
+    "Optic Tunning 1" : "+2.90",
+    "Optic Tunning 2" : "-1.65\n",
+    # Underbarrel
+    "Underbarrel" : "FTAC Ripper 56",
+    "Underbarrel Tunning 1" : "+0.65",
+    "Underbarrel Tunning 2" : "+0.37\n",
+    # Magazine
+    "Magazine" : "60 Round Mag",
+}
+
+# Chimera
+chimera = {
+    # Info
+    "Name" : "Chimera",
+    "Gun Type" : "AR",
+    "Gun Platform" : "Bruen Ops",
+    "Tier" : "A\n",
+    
+    # Muzzle
+    "Muzzle" : 'Sakin Thread-40',
+    "Muzzle Tunning 1" : "+0.52",
+    "Muzzle Tunning 2" : "+0.23\n",
+    # Barrel
+    "Barrel" : '6.5" EXF Vorpal',
+    "Barrel Tunning 1" : "+0.34",
+    "Barrel Tunning 2" : "+0.26\n",
+    # Underbarrel
+    "Underbarrel" : "Edge-47 Grip",
+    "Underbarrel Tunning 1" : "-0.41",
+    "Underbarrel Tunning 2" : "-0.21\n",
+    # Magazine
+    "Magazine" : "45 Round Mag",
+    # Rear Grip
+    "Rear Grip" : "Bruen Flash Grip",
+    "Rear Grip Tunning 1" : "+0.65",
+    "Rear Grip Tunning 2" : "-0.28\n",
+}
+
+# Kastov-74U
+kastov_74U = {
+    # Info
+    "Name" : "Chimera",
+    "Gun Type" : "AR",
+    "Gun Platform" : "Kastovia",
+    "Tier" : "A\n",
+    
+    # Barrel
+    "Barrel" : 'BR209 Barrel',
+    "Barrel Tunning 1" : "+0.18",
+    "Barrel Tunning 2" : "-0.19\n",
+    # Barrel
+    "Laser" : 'FSS OLE-V Laser',
+    "Laser Tunning 1" : "-0.24",
+    "Laser Tunning 2" : "-27.97\n",
+    # Stock
+    "Stock" : "Markeev R7 Stock",
+    "Stock Tunning 1" : "-2.19",
+    "Stock Tunning 2" : "-1.32\n",
+    # Magazine
+    "Magazine" : "45 Round Mag",
+    # Rear Grip
+    "Rear Grip" : "Demo-X2 Grip",
+    "Rear Grip Tunning 1" : "-0.65",
+    "Rear Grip Tunning 2" : "-0.22\n",
 }
